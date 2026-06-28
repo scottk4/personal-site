614fb4e9ba521d3a76c4108d21630372d9ab2c7e
Scott Kelly <scottakelly2@gmail.com>
2026-06-28 17:49:15 -0600

    added astro preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://app.netlify.com/projects/glittering-crepe-66d2cc/overview"
+  site: "https://app.netlify.com/projects/glittering-crepe-66d2cc/overview",
+  integrations: [preact()]
 });
